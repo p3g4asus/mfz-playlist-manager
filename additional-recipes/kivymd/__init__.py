adf96daa94a07b348190eeb0423831843834476a
Matteo Federico Zazzetta <fulminedipegasus@gmail.con>
2020-03-08 14:09:01 +0100

    Slightly changed kivymd recipe

--- additional-recipes/kivymd/__init__.py
+++ additional-recipes/kivymd/__init__.py
@@ -1,6 +1,6 @@
 from pythonforandroid.recipe import PythonRecipe
 class KivyMDRecipe(PythonRecipe):
-    # version = '6abb482b2ab4934e34e03b19b4a0b6dcd639b63a'
+    # version = 'master'
     # url = 'https://github.com/HeaTTheatR/KivyMD/archive/{version}.zip'
     version = '27e7e35576ec14d1fa11973a86d85ed8657eb7ae'
     url = 'https://github.com/p3g4asus/KivyMD/archive/{version}.zip'
