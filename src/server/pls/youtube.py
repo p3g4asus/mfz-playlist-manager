6364269e45dd46572cc2e15f18ab76304c61fa95
Matteo Federico Zazzetta <fulminedipegasus@gmail.com>
2021-03-07 10:45:48 +0100

    Fixes for youtube

--- src/server/pls/youtube.py
+++ src/server/pls/youtube.py
@@ -4,11 +4,8 @@ import traceback
 import youtube_dl
 from datetime import (datetime, timedelta)
 
-import aiohttp
-
 from common.const import (CMD_YT_PLAYLISTCHECK, MSG_YT_INVALID_PLAYLIST,
-                          MSG_YT_INVALID_CHANNEL, MSG_BACKEND_ERROR,
-                          MSG_NO_VIDEOS)
+                          MSG_BACKEND_ERROR, MSG_NO_VIDEOS)
 from common.playlist import PlaylistItem
 
 from .refreshmessageprocessor import RefreshMessageProcessor
@@ -26,136 +23,55 @@ class MessageProcessor(RefreshMessageProcessor):
 
     @staticmethod
     def programsUrl(plid):
-        return f'https://m.youtube.com/playlist?list={plid}' if plid[0] != '&' else\
-            f'https://m.youtube.com/watch?v={plid[1:]}'
-
-    @staticmethod
-    def channelUrl(user, vers):
-        if vers == 2:
-            return f'https://www.youtube.com/c/{user}/videos'
-        elif vers == 3:
-            return f'https://www.youtube.com/channel/{user}/videos'
+        if plid[0] == '&':
+            return f'https://m.youtube.com/watch?v={plid[1:]}'
+        elif plid[0] == '|':
+            return f'https://www.youtube.com/channel/{plid[1:]}/videos'
         else:
-            return f'https://www.youtube.com/user/{user}/videos'
-
-    @staticmethod
-    def channelIdUrl(chanid):
-        return f'https://www.youtube.com/channel/{chanid}'
-
-    async def channelid2user(self, session, chanid):
-        url = MessageProcessor.channelIdUrl(chanid)
-        _LOGGER.debug("Youtube: Getting processPlaylistCheck " + url)
-        async with session.get(
-                url,
-                headers={'User-Agent': r'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}) as resp:
-            if resp.status == 200:
-                txt = await resp.text()
-                # _LOGGER.debug("Received " + txt)
-                mo = re.search(r'/(?:c|user)/([^/]+)/videos', txt)
-                if mo:
-                    return mo.group(1)
-                else:
-                    return 15
-            else:
-                return 14
-
-    async def channel2playlist(self, session, chanid, vers):
-        url = MessageProcessor.channelUrl(chanid, vers)
-        _LOGGER.debug("Youtube: Getting processPlaylistCheck " + url)
-        async with session.get(
-                url,
-                headers={'User-Agent': r'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}) as resp:
-            if resp.status == 200:
-                txt = await resp.text()
-                # _LOGGER.debug("Received " + txt)
-                mo = re.search(r'/playlist\?list=([^&\\]+)', txt)
-                if mo:
-                    return mo.group(1)
-                else:
-                    return 13
-            else:
-                return 12
+            return f'https://m.youtube.com/playlist?list={plid}'
 
     async def processPlaylistCheck(self, msg, userid, executor):
         text = msg.f('text', (str,))
         if text:
             try:
                 plid = ''
-                async with aiohttp.ClientSession() as session:
-                    mo2 = re.search(r'v=([^&?/]+)', text)
-                    if mo2:
-                        plid = "&" + mo2.group(1)
-                    else:
-                        mo2 = re.search(r'list=([^&]+)', text)
-                        if mo2:
-                            plid = mo2.group(1)
-                        else:
-                            vers = None
-                            channelfound = re.search(r'/channel/([^/?&]+)', text)
-                            if channelfound:
-                                res = await self.channelid2user(session, channelfound.group(1))
-                                if isinstance(res, int):
-                                    chanid = channelfound.group(1)
-                                    vers = 3
-                                else:
-                                    chanid = res
-                            userfound = re.search(r'/c/([^/?&]+)', text)
-                            if userfound:
-                                chanid = userfound.group(1)
-                            elif not channelfound:
-                                mo1 = re.search(r'/([^/]+)$', text)
-                                if mo1:
-                                    chanid = mo1.group(1)
-                                else:
-                                    chanid = text
-                            if vers:
-                                res = await self.channel2playlist(session, chanid, vers)
-                            else:
-                                res = await self.channel2playlist(session, chanid, 2)
-                                if isinstance(res, int):
-                                    res = await self.channel2playlist(session, chanid, 1)
-                            if isinstance(res, int):
-                                if userfound or channelfound:
-                                    return msg.err(res, MSG_YT_INVALID_CHANNEL)
-                                else:
-                                    plid = chanid
-                            else:
-                                plid = res
+                mo2 = re.search(r'v=([^&?/]+)', text)
+                if mo2:
+                    plid = "&" + mo2.group(1)
                     url = MessageProcessor.programsUrl(plid)
-                    _LOGGER.debug("Youtube: Getting processPlaylistCheck " + url)
-                    ydl_opts = {
-                        'ignoreerrors': True,
-                        'quiet': True,
-                        'playliststart': 1,
-                        'playlistend': 100,
-                        'extract_flat': True
-                    }
-                    playlist_dict = dict()
-                    plinfo = dict()
-                    try:
-                        await executor(self.youtube_dl_get_dict, url, ydl_opts, playlist_dict)
-                        if '_err' not in playlist_dict:
-                            if plid[0] != '&':
-                                plinfo = dict(
-                                    title=playlist_dict['title'],
-                                    channel=playlist_dict['uploader'],
-                                    id=plid,
-                                    description=playlist_dict['title']
-                                )
-                            else:
-                                plinfo = dict(
-                                    title=playlist_dict['title'],
-                                    channel=playlist_dict['uploader'],
-                                    id=plid,
-                                    description=playlist_dict['description']
-                                )
-                        if plinfo:
-                            return msg.ok(playlistinfo=plinfo)
-                        else:
-                            return msg.err(15, MSG_YT_INVALID_PLAYLIST)
-                    except Exception:
-                        _LOGGER.error(traceback.format_exc())
-                        return msg.err(16, MSG_YT_INVALID_PLAYLIST)
+                else:
+                    mo2 = re.search(r'list=([^&?/]+)', text)
+                    if mo2:
+                        plid = mo2.group(1)
+                        url = MessageProcessor.programsUrl(plid)
+                    else:
+                        mo2 = re.search(r'/videos$', text)
+                        url = text if mo2 else text + '/videos'
+                _LOGGER.debug("Youtube: Getting processPlaylistCheck " + url)
+                ydl_opts = {
+                    'ignoreerrors': True,
+                    'quiet': True,
+                    'playliststart': 1,
+                    'playlistend': 100,
+                    'extract_flat': True
+                }
+                playlist_dict = dict()
+                plinfo = dict()
+                try:
+                    await executor(self.youtube_dl_get_dict, url, ydl_opts, playlist_dict)
+                    if '_err' not in playlist_dict:
+                        plinfo = dict(
+                            title=playlist_dict['title'],
+                            channel=playlist_dict['uploader'],
+                            id=plid if plid else "|" + playlist_dict['id'],
+                            description=playlist_dict.get('description', playlist_dict['title'])
+                        )
+                        return msg.ok(playlistinfo=plinfo)
+                    else:
+                        return msg.err(15, MSG_YT_INVALID_PLAYLIST)
+                except Exception:
+                    _LOGGER.error(traceback.format_exc())
+                    return msg.err(16, MSG_YT_INVALID_PLAYLIST)
             except Exception:
                 _LOGGER.error(traceback.format_exc())
                 return msg.err(11, MSG_BACKEND_ERROR)
