86bf926e7578d4ea53d429acffa1a3f85dc921d8
p3g4asus <fulminedipegasus@gmail.com>
2022-06-02 19:42:01 +0200

    Renaming google-client-id file

--- server.sh
+++ server.sh
@@ -1,7 +1,7 @@
 PTH=$( cd "$( dirname "${BASH_SOURCE:-$0}" )" && pwd )
 source $PTH/venv/bin/activate
 apik=$(head -n 1 youtube-apikey.txt)
-cid=$(head -n 1 google-client_id.txt)
+cid=$(head -n 1 google-client-id.txt)
 cd "$PTH/src"
 FILEOUT=$PTH/logs/`date "+%Y%m%d"`_pm.out
 ( python -m server --dbfile ../main.sqlite --static ./www --port 5802 --client-id "$cid" --youtube-apikey "$apik" --autoupdate 3 -v ) > $FILEOUT 2>&1
