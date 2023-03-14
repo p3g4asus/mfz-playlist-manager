689f6d2261237ff82e9ae2080fb843c2b38bbe34
p3g4asus <3275734+p3g4asus@users.noreply.github.com>
2023-03-14 14:55:15 +0100

    Wrong video id in twitch video info request

--- src/server/twitch_vod_link2.py
+++ src/server/twitch_vod_link2.py
@@ -7,7 +7,7 @@ _LOGGER = logging.getLogger(__name__)
 
 
 async def get_vod_link(vodid):
-    json = '[{"operationName": "VideoMetadata", "variables": {"channelLogin": "", "videoID": "%d"}, "extensions": {"persistedQuery": {"version": 1, "sha256Hash": "49b5b8f268cdeb259d75b58dcb0c1a748e3b575003448a2333dc5cdafd49adad"}}}, {"operationName": "VideoPlayer_ChapterSelectButtonVideo", "variables": {"includePrivate": false, "videoID": "1762458143"}, "extensions": {"persistedQuery": {"version": 1, "sha256Hash": "8d2793384aac3773beab5e59bd5d6f585aedb923d292800119e03d40cd0f9b41"}}}, {"operationName": "VideoPlayer_VODSeekbarPreviewVideo", "variables": {"includePrivate": false, "videoID": "1762458143"}, "extensions": {"persistedQuery": {"version": 1, "sha256Hash": "07e99e4d56c5a7c67117a154777b0baf85a5ffefa393b213f4bc712ccaf85dd6"}}}]' % vodid
+    json = '[{"operationName": "VideoMetadata", "variables": {"channelLogin": "", "videoID": "%d"}, "extensions": {"persistedQuery": {"version": 1, "sha256Hash": "49b5b8f268cdeb259d75b58dcb0c1a748e3b575003448a2333dc5cdafd49adad"}}}, {"operationName": "VideoPlayer_ChapterSelectButtonVideo", "variables": {"includePrivate": false, "videoID": "%d"}, "extensions": {"persistedQuery": {"version": 1, "sha256Hash": "8d2793384aac3773beab5e59bd5d6f585aedb923d292800119e03d40cd0f9b41"}}}, {"operationName": "VideoPlayer_VODSeekbarPreviewVideo", "variables": {"includePrivate": false, "videoID": "%d"}, "extensions": {"persistedQuery": {"version": 1, "sha256Hash": "07e99e4d56c5a7c67117a154777b0baf85a5ffefa393b213f4bc712ccaf85dd6"}}}]' % (vodid, vodid, vodid)
 
     url = "https://gql.twitch.tv/gql"
     headers = {'content-type': "text/plain;charset=UTF-8", "Client-ID": "kimne78kx3ncx6brgo4mv6wki5h1ko"}
