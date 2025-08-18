5802a43055c231f20bac243da391e9412fcd03f4
p3g4asus <3275734+p3g4asus@users.noreply.github.com>
2025-08-18 17:10:58 +0200

    Better tab extension option visualization Fix auto resize based on screen size

--- mfz-remote-tabs/firefox/tabs.js
+++ mfz-remote-tabs/firefox/tabs.js
@@ -204,3 +204,41 @@ browser.tabs.onMoved.addListener((tabId, moveInfo) => {
 document.addEventListener('DOMContentLoaded', () => {
     listTabs();
 });
+
+const urlInput = document.querySelector('#url');
+const urlbtn = document.querySelector('#regbutton');
+
+
+/*
+Store the currently selected settings using browser.storage.local.
+*/
+function storeSettings() {
+    browser.storage.local.set({
+        tabControl: {
+            url: urlInput.value
+        }
+    });
+}
+
+/*
+Update the options UI with the settings values retrieved from storage,
+or the default settings if the stored settings are empty.
+*/
+function updateUI(restoredSettings) {
+    urlInput.value = restoredSettings?.tabControl?.url || '';
+}
+
+function onError(e) {
+    console.error(e);
+}
+
+/*
+On opening the options page, fetch stored settings and update the UI with them.
+*/
+const gettingStoredSettings = browser.storage.local.get();
+gettingStoredSettings.then(updateUI, onError);
+
+/*
+On blur, save the currently selected settings.
+*/
+urlbtn.addEventListener('click', storeSettings);
--- src/www/play/video_manager.js
+++ src/www/play/video_manager.js
@@ -1104,10 +1104,12 @@ function video_resize() {
     if (!video_manager_obj)
         return;
     let new_height, new_width;
+    const wiw = Math.min(window.innerWidth, window.screen.availWidth);
+    const wih = Math.min(window.innerHeight, window.screen.availHeight);
     let ttlw = $('#video_title').height() + 25 + $('#prev_button').height(); /* title + buttons */
-    if (video_height + ttlw > window.innerHeight || video_width > window.innerWidth) {
-        new_height = window.innerHeight - ttlw;
-        new_width = window.innerWidth;
+    if (video_height + ttlw > wih || video_width > wiw) {
+        new_height = wih - ttlw;
+        new_width = wiw;
         if (new_height < 160 || new_width < 284) {
             new_height = 160;
             new_width = 284;
